36a58bcffddcf2f600a8d8c82d9f3dfb852f99e9
Naohiro Hirata <hirata.acc@gmail.com>
2021-02-07 20:23:29 +0900

    CH3:p31_locals変数を使うとコマンド実行時に上書きできない

--- CH3/main.tf
+++ CH3/main.tf
@@ -1,8 +1,7 @@
-variable "example_instance_type" {
-    default = "t3.micro"
+locals {
+    example_instance_type = "t3.micro"
 }
-
 resource "aws_instance" "example" {
     ami = "ami-0cfd0f5d33134a76"
-    instance_type = "var.example_instance_type"
+    instance_type = local.example_instance_type
 }
